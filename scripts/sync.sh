066db4f4d660bf1ce8ade203c65d2620097da54a
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2019-09-02 23:51:10 +0530

    Fix indents and make few more corrections

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -12,45 +12,45 @@
 command="$1"
 
 if [ "$command" == "Repo Initialization" ];
+then
+   export repo=".repo"
+   if [ ! -d "$repo" ];
    then
-       export repo=".repo"
-       if [ ! -d "$repo" ];
-       then
-             echo -e "Initializing The Repo"
-             repo init -u https://github.com/PixysOS/manifest.git -b pie
-             sleep 5
-             echo -e "Repo has been Initialized, please sync the source"
-	   fi
-	   echo -e "Repo Already Initialized, please Remove the .repo Directory or Just Start Sync The Source";
+      echo -e "Initializing The Repo"
+      repo init -u https://github.com/PixysOS/manifest.git -b pie
+      sleep 5
+      echo -e "Repo has been Initialized, please sync the source"
+   fi
+   echo -e "Repo Already Initialized, please Remove the .repo Directory or Just Start Sync The Source";
 fi
 
 
 if [ "$command" == "Json" ];
+then
+   export DIRECTORY=json;
+   if [ ! -d "$DIRECTORY" ];
    then
-       export DIRECTORY=json;
-       if [ ! -d "$DIRECTORY" ];
-       then
-             echo -e "Creating json Directory"
-       	     mkdir "$DIRECTORY" ;
-			 echo -e "Json Directory Created"
-       fi
-	   echo -e "json Directory Already Exist, Exiting Now.";
+      echo -e "Creating json Directory"
+      mkdir "$DIRECTORY"
+      echo -e "Json Directory Created"
+   fi
+   echo -e "json Directory Already Exist, Exiting Now.";
 fi
 
 if [ "$command" == "Sync Source" ];
-   then
-        echo -e "Syncing Source, will take Little Time."
-		repo sync --force-sync -j48 --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune
-		wait
-		echo "Source Synced Successfully"
+then
+   echo -e "Syncing Source, will take Little Time."
+   repo sync --force-sync -j48 --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune
+   sleep 5
+   echo "Source Synced Successfully"
 fi
 
 if [ "$command" == "Sync Source and Json" ];
-   then
-        echo -e "Syncing Source, will take Little Time."
-		repo sync --force-sync -j48 --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune
-		wait
-		echo -e "Source Synced Successfully"
-		mkdir json
-		echo -e "Json Directory Created"
+then
+   echo -e "Syncing Source, will take Little Time"
+   repo sync --force-sync -j48 --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune
+   sleep 5
+   echo -e "Source Synced Successfully"
+   mkdir json
+   echo -e "Json Directory Created"
 fi
