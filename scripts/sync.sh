92017b819f9b147bfb162d63c9faefb5c33cfd41
Subins Mani <subins.mani@gmail.com>
2019-10-11 13:55:52 +0530

    Lets prepare our jenkins for Android 10

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -17,7 +17,7 @@ then
    if [ ! -d "$repo" ];
    then
       echo -e "Initializing The Repo"
-      repo init -u https://github.com/PixysOS/manifest.git -b pie
+      repo init -u https://github.com/PixysOS/manifest.git -b ten
       sleep 5
       echo -e "Repo has been Initialized, please sync the source"
    fi
