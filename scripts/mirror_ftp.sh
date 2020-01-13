aedbb7af03f23979df05ee67ee90d0ac173faa96
Subins Mani <subins.mani@gmail.com>
2020-01-13 15:52:17 +0530

    Make Changes for PixysOS 3.0

--- scripts/mirror_ftp.sh
+++ scripts/mirror_ftp.sh
@@ -12,19 +12,19 @@
 DEVICE=${1}
 
 function TGlogs() {
-    curl -s "https://api.telegram.org/bot${bottoken}/sendmessage" --data "text=${*}&chat_id=-1001322414571&parse_mode=Markdown" > /dev/null
+    curl -s "https://api.telegram.org/bot${bottoken}/sendmessage" --data "text=${*}&chat_id=-1001144148166&parse_mode=Markdown" > /dev/null
 }
 
 function mirror() {
-   git clone https://github.com/PixysOS-Devices/official_devices devices
-   cd devices/${DEVICE}
-   FILENAME=$(jq -r '.response[].filename' build.json)
-   cd ../..
    wget https://downloads.pixysos.com/.test/${DEVICE}/${FILENAME}
    ZIP=$(ls PixysOS*.zip)
    export spass
-   sshpass -p "${spass}" scp -o StrictHostKeyChecking=no "${ZIP}" pshreejoy15@frs.sourceforge.net:/home/frs/project/pixys-os/pie/${DEVICE}
-   TGlogs "${FILENAME} has been mirrored to PixysOS [sourceforge portal](https://sourceforge.net/projects/pixys-os/files/pie/${DEVICE})"
+   export opass
+   sshpass -p "${opass}" scp -P 5615 -o StrictHostKeyChecking=no "${ZIP}" root@uploads.pixysos.com:/home/ftp/uploads/${DEVICE}/ten/
+   TGlogs "${FILENAME} has been Uploaded to PixysOS [official download portal](https://downloads.pixysos.com/${DEVICE}/ten/)"
+   sshpass -p "${spass}" scp -o StrictHostKeyChecking=no "${ZIP}" pixysuploads@frs.sourceforge.net:/home/frs/project/pixys-os/ten/${DEVICE}/
+   TGlogs "${FILENAME} has been mirrored to PixysOS [sourceforge portal](https://sourceforge.net/projects/pixys-os/files/ten/${DEVICE})"
+   rm -rf *.zip
 }
 
 mirror
