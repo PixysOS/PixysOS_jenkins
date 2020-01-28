9a4f9150638bce0261bfa6f9f274b6c097a57c56
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2020-01-28 21:07:23 +0530

    scripts: Add minor fixes to scripts and pretty-ify

--- scripts/org-management.sh
+++ scripts/org-management.sh
@@ -10,139 +10,127 @@
 # PixysOS ROM building script.
 
 function telegram() {
-   curl -s "https://api.telegram.org/bot${telegram_bot_token}/sendmessage" --data "text=${*}&chat_id=${chat_id}&parse_mode=HTML"
-}  
- 
+	curl -s "https://api.telegram.org/bot${telegram_bot_token}/sendmessage" --data "text=${*}&chat_id=${chat_id}&parse_mode=HTML"
+}
+
 # Function to upload to del.dog
 function deldog() {
-    RESULT=$(curl -sf --data-binary @"${1:--}" https://del.dog/documents) || {
-        echo "ERROR: failed to post document" >&2
-        exit 1
-    }
-    KEY=$(jq -r .key <<< "${RESULT}")
-    echo "https://del.dog/raw/${KEY}"
+	RESULT=$(curl -sf --data-binary @"${1:--}" https://del.dog/documents) || {
+		echo "ERROR: failed to post document" >&2
+		exit 1
+	}
+	KEY=$(jq -r .key <<<"${RESULT}")
+	echo "https://del.dog/raw/${KEY}"
 }
 
 function exit-process() {
-   paste_url=$(deldog "${log_file}")
-   cat "${log_file}"
-   BUILD_END=$(date +"%s")
-   DIFF=$((BUILD_END - BUILD_START))
-   telegram "<b>Organization maintainance completed</b>
-
+	paste_url=$(deldog "${log_file}")
+	cat "${log_file}"
+	BUILD_END=$(date +"%s")
+	DIFF=$((BUILD_END - BUILD_START))
+	telegram "<b>Organization maintainance completed</b>
    <b>Status</b> : ${status}
    <b>Logs</b> : <a href=\"${paste_url}\">click-here</a>
    <b>Time taken</b> : ${DIFF} seconds"
 
-  if [[ "$status" == "failed" ]]
-  then 
-     exit 1
-  fi
-  exit 0
+	if [[ "$status" == "failed" ]]; then
+		exit 1
+	fi
+	exit 0
 }
 
 function log() {
-   echo "${*}"
-   echo "${*}" > "${log_file}"
+	echo "${*}"
+	echo "${*}" >"${log_file}"
 }
 
 function check-repo() {
-   response=$(curl -X GET -H "Authorization: token ${github_token}" -s https://api.github.com/repos/"${org_name}"/"$repo_name")
-   check=$(jq -r '.message' <<< "$response")
-   verify=$(jq -r '.name' <<< "$response")
-   if [[ $check == "Not Found" ]] && [[ -z $verify ]]
-   then
-      if [[ $user_action == "Nothing" ]] || [[ $user_action == "Add" ]]
-	  then
-             log "$(date) => $repo_name not found on ${org_name} GitHub organization, Creating $repo_name"
-	     create-repo
-	     [[ $user_action == "Add" ]] && add-users
-	  elif [[ $user_action == "Remove" ]]
-	  then
-	     log "$(date) => $repo_name not found on ${org_name} GitHub organization, Cannot remove user(s) $users"
-	     status="failed"
-	  fi
-   else
-      log "$(date) => $repo_name found on $org_name, Proceeding with further action"
-      if [[ $repo_action == "Remove" ]]
-	  then
-	     delete-repo
-	  fi
-   fi	  
+	response=$(curl -X GET -H "Authorization: token ${github_token}" -s https://api.github.com/repos/"${org_name}"/"$repo_name")
+	check=$(jq -r '.message' <<<"$response")
+	verify=$(jq -r '.name' <<<"$response")
+	if [[ $check == "Not Found" ]] && [[ -z $verify ]]; then
+		if [[ $user_action == "Nothing" ]] || [[ $user_action == "Add" ]]; then
+			log "$(date) => $repo_name not found on ${org_name} GitHub organization, Creating $repo_name"
+			create-repo
+			[[ $user_action == "Add" ]] && add-users
+		elif [[ $user_action == "Remove" ]]; then
+			log "$(date) => $repo_name not found on ${org_name} GitHub organization, Cannot remove user(s) $users"
+			status="failed"
+		fi
+	else
+		log "$(date) => $repo_name found on $org_name, Proceeding with further action"
+		if [[ $repo_action == "Remove" ]]; then
+			delete-repo
+		fi
+	fi
 }
 
 function create-repo() {
-   response=$(curl -X POST -H "Authorization: token ${github_token}" --data '{ "name":"'"$repo_name"'"}' https://api.github.com/orgs/"${org_name}"/repos)
-   verify=$(jq -r '.name' <<< "$response")
-   
-   if [[ -n $verify ]] 
-   then 
-      log "$(date) => New repository created : https://github.com/${org_name}/$repo_name"
-   else 
-      log "$(date) => Fatal error: Cannot create $repo_name" && status="failed"
-   fi
+	response=$(curl -X POST -H "Authorization: token ${github_token}" --data '{ "name":"'"$repo_name"'"}' https://api.github.com/orgs/"${org_name}"/repos)
+	verify=$(jq -r '.name' <<<"$response")
+
+	if [[ -n $verify ]]; then
+		log "$(date) => New repository created : https://github.com/${org_name}/$repo_name"
+	else
+		log "$(date) => Fatal error: Cannot create $repo_name" && status="failed"
+	fi
 }
 
 function delete-repo() {
-   response=$(curl -X DELETE -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name")
-   # Check if the repo is deleted or not.
-   response=$(curl -X GET -H "Authorization: token ${github_token}" -s https://api.github.com/repos/"${org_name}"/"$repo_name")
-   check=$(jq -r '.message' <<< "$response")
-   verify=$(jq -r '.name' <<< "$response")
-   if [[ $check == "Not Found" ]] && [[ -z $verify || $verify == "null" ]]
-   then
-       log "$(date) => Repository named \"${repo_name}\" deleted successfully from ${org_name} GitHub Organization"
-   else
-       log "$(date) => Fatal error deleting \"${repo_name}\" from ${org_name} GitHub Organization"
-   fi      
+	response=$(curl -X DELETE -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name")
+	# Check if the repo is deleted or not.
+	response=$(curl -X GET -H "Authorization: token ${github_token}" -s https://api.github.com/repos/"${org_name}"/"$repo_name")
+	check=$(jq -r '.message' <<<"$response")
+	verify=$(jq -r '.name' <<<"$response")
+	if [[ $check == "Not Found" ]] && [[ -z $verify || $verify == "null" ]]; then
+		log "$(date) => Repository named \"${repo_name}\" deleted successfully from ${org_name} GitHub Organization"
+	else
+		log "$(date) => Fatal error deleting \"${repo_name}\" from ${org_name} GitHub Organization"
+	fi
 }
 
 function add-users() {
-   while IFS= read -r user
-   do
-      user=$(echo -e "${user}" | tr -d '[:space:]')
-      if [[ -n $user ]]
-      then
-         response=$(curl -s https://api.github.com/users/"$user")
-	 check=$(jq -r '.message' <<< "$response")
-	 verify=$(jq -r '.login' <<< "$response")
-	 if [[ $check != "Not Found" ]] && [[ $verify == "$user" ]]
-	 then
-	  response=$(curl -X GET -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name/collaborators/$user")
-	  verify=$(jq -r '.message' <<< "$response")
-	  if [[ $verify == "Not Found" ]]
-	  then
-	     response=$(curl -X PUT -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name/collaborators/$user")
-	     log "$(date) => \"$user\" has been invited as a collaborater in \"$repo_name\" repository."
-	  else
-	     log "$(date) => Warning: \"$user\" is already a collaborater in \"$repo_name\" repository.  Process \"add-users\" aborted"
-	  fi
-   done <<< "$users"
+	while IFS= read -r user; do
+		user=$(echo -e "${user}" | tr -d '[:space:]')
+		if [[ -n $user ]]; then
+			response=$(curl -s https://api.github.com/users/"$user")
+			check=$(jq -r '.message' <<<"$response")
+			verify=$(jq -r '.login' <<<"$response")
+			if [[ $check != "Not Found" ]] && [[ $verify == "$user" ]]; then
+				response=$(curl -X GET -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name/collaborators/$user")
+				verify=$(jq -r '.message' <<<"$response")
+				if [[ $verify == "Not Found" ]]; then
+					response=$(curl -X PUT -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name/collaborators/$user")
+					log "$(date) => \"$user\" has been invited as a collaborater in \"$repo_name\" repository."
+				else
+					log "$(date) => Warning: \"$user\" is already a collaborater in \"$repo_name\" repository.  Process \"add-users\" aborted"
+				fi
+			fi
+		fi
+	done <<<"$users"
 }
 
 function remove-users() {
-   while IFS= read -r user
-   do
-      user=$(echo -e "${user}" | tr -d '[:space:]')
-      if [[ -n $user ]]
-      then
-         response=$(curl -s https://api.github.com/users/"$user")
-	 check=$(jq -r '.message' <<< "$response")
-	 verify=$(jq -r '.login' <<< "$response")
-	 if [[ $check != "Not Found" ]] && [[ $verify == "$user" ]]
-	 then
-	  response=$(curl -X GET -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name/collaborators/$user")
-	  verify=$(jq -r '.message' <<< "$response")
-	  if [[ $verify != "Not Found" ]]
-	  then
-	     response=$(curl -X DELETE -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name/collaborators/$user")
-	     log "$(date) => \"$user\" has been removed from collaboraters in \"$repo_name\" repository."
-	  else
-	     log "$(date) => Warning: \"$user\" is not a collaborater in \"$repo_name\" repository. Process \"remove-users\" aborted"
-	  fi
-   done <<< "$users"
+	while IFS= read -r user; do
+		user=$(echo -e "${user}" | tr -d '[:space:]')
+		if [[ -n $user ]]; then
+			response=$(curl -s https://api.github.com/users/"$user")
+			check=$(jq -r '.message' <<<"$response")
+			verify=$(jq -r '.login' <<<"$response")
+			if [[ $check != "Not Found" ]] && [[ $verify == "$user" ]]; then
+				response=$(curl -X GET -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name/collaborators/$user")
+				verify=$(jq -r '.message' <<<"$response")
+				if [[ $verify != "Not Found" ]]; then
+					response=$(curl -X DELETE -H "Authorization: token ${github_token}" -s "https://api.github.com/repos/${org_name}/$repo_name/collaborators/$user")
+					log "$(date) => \"$user\" has been removed from collaboraters in \"$repo_name\" repository."
+				else
+					log "$(date) => Warning: \"$user\" is not a collaborater in \"$repo_name\" repository. Process \"remove-users\" aborted"
+				fi
+			fi
+		fi
+	done <<<"$users"
 }
- 
+
 # Organization details
 org_name="PixysOS-Devices"
 # Repository details
@@ -158,12 +146,10 @@ chat_id=${7}
 status="passed"
 # Log file details
 log_file=$(mktemp)
-console_dump=$(mktemp)
 
 BUILD_START=$(date +"%s")
-while IFS= read -r repo_name
-do
-  log "$(date) => Starting process for $repo_name"
-  check-repo
-done <<< "$repo_names"
+while IFS= read -r repo_name; do
+	log "$(date) => Starting process for $repo_name"
+	check-repo
+done <<<"$repo_names"
 exit-process
