1d13e4e94cd90d7b60cc1aebb90dbc9a9f76ff38
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2020-03-07 10:52:37 +0530

    [WIP] Initial push

--- scripts/new_upload.py
+++ scripts/new_upload.py
@@ -1,1 +1,25 @@
-#Placeholder
+import json
+import rclone
+import requests
+import wget
+
+onedrive_cfg_url = os.environ("onedrive_cfg_url")
+onedrive_cfg = requests.get(cfg_url).text
+
+sourceforge_cfg_url = os.environ("sf_cfg_url")
+sourceforge_cfg = requests.get(sourceforge_cfg_url).text
+
+def get_build():
+  result = rclone.with_config(onedrive_cfg).run_cmd(command="link", extra_args=[f"Onedrive:Pixysos-test/{DEVICE}/{FOLDER}/{FILENAME}"])
+  temp_link = result.get('out').decode("utf-8")
+  fileid = temp_link.split("/")[4]
+  link = f'http://seleniums.herokuapp.com/try/{fileid}/d'
+  wget.download(link, f'{FILENAME}')
+  
+def buildapi:
+def upload:
+def telegram:
+  
+  
+  
+  
