024880efceb5ca341169776c7dabb6e7c337d238
Shreejoy Dash <39165840+shreejoy@users.noreply.github.com>
2020-03-07 21:56:03 +0530

    progress a bit more still [WIP]

--- scripts/new_upload.py
+++ scripts/new_upload.py
@@ -1,25 +1,51 @@
-import json
-import rclone
-import requests
-import wget
+import json, wget, requests, rclone
+from os import environ
+from pathlib import Path
 
-onedrive_cfg_url = os.environ("onedrive_cfg_url")
-onedrive_cfg = requests.get(cfg_url).text
+# Onedrive config
+onedrive_cfg_url = environ.get("onedrive_cfg_url")
+onedrive_cfg = requests.get(onedrive_cfg_url).text
 
-sourceforge_cfg_url = os.environ("sf_cfg_url")
+# Sourceforge config
+sourceforge_cfg_url = environ.get("sf_cfg_url")
 sourceforge_cfg = requests.get(sourceforge_cfg_url).text
 
+# Needed build parameters
+DEVICE = environ.get("DEVICE")
+FOLDER = environ.get("FOLDER")
+FILENAME = environ.get("FILENAME")
+
 def get_build():
+  # Get the download link of the file
   result = rclone.with_config(onedrive_cfg).run_cmd(command="link", extra_args=[f"Onedrive:Pixysos-test/{DEVICE}/{FOLDER}/{FILENAME}"])
-  temp_link = result.get('out').decode("utf-8")
-  fileid = temp_link.split("/")[4]
+  # Create the download link
+  if (result.get('error') != 0):
+    print(f"{FILENAME} is not existing in test folder")
+    exit(1)
+  # As the indirect link is generated means the file exits on the server so get the fileid to process further
+  fileid = result.get('out').decode("utf-8").split("/")[4]
   link = f'http://seleniums.herokuapp.com/try/{fileid}/d'
+  # Download the file
   wget.download(link, f'{FILENAME}')
+  # To check if the file was downloaded or not
+  if Path(f'{FILENAME}').is_file():
+    print ("File downloaded properly now uploading!")
+    buildapi()
+    upload()
+  else:
+    print ("File not downloaded properly")
+    exit(1)
   
 def buildapi:
-def upload:
-def telegram:
-  
+def upload():
+  result = rclone.with_config(onedrive_cfg).run_cmd(command="copy", extra_args=[f"{FILENAME}", f"Onedrive:PixysOS/{DEVICE}/{FOLDER}"])
+  if (result.get('error') != 0):
+    print("Upload task failed")
+    exit(1)
+  result = rclone.with_config(sourceforge_cfg).run_cmd(command="copy", extra_args=[f"{FILENAME}", f"Sourceforge:PixysOS/{DEVICE}"])
+  if (result.get('error') != 0):
+    print("Upload task failed")
+    exit(1)
   
   
   
