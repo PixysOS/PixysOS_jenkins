0f003013442db1c82be65eccde31b65f54da53d0
Subins Mani <subins.mani@gmail.com>
2020-03-27 12:32:13 +0530

    Fix FTP Service for Mirror Job

--- scripts/mirror_ftp.sh
+++ scripts/mirror_ftp.sh
@@ -12,7 +12,7 @@
 DEVICE=${1}
 FILENAME=${2}
 
-export spass opass bottoken
+export opass bottoken
 
 function TG() {
     curl -s "https://api.telegram.org/bot${bottoken}/sendmessage" --data "text=${*}&chat_id=-1001144148166&parse_mode=HTML" > /dev/null
@@ -23,8 +23,8 @@ function mirror() {
    wget https://downloads.pixysos.com/.test/"${DEVICE}"/${FOLDER}/"${FILENAME}"
    CHECK=$(ls PixysOS*.zip)
    [ "${CHECK}" == "${FILENAME}" ] && echo "${FILENAME} found, Starting upload process" || TG "$FILENAME cannot be downloaded correctly"
-   sshpass -p "${opass}" scp -P 5615 -o StrictHostKeyChecking=no "${FILENAME}" root@uploads.pixysos.com:/home/ftp/uploads/"${DEVICE}"/ten/
-   sshpass -p "${spass}" scp -o StrictHostKeyChecking=no "${FILENAME}" pixysuploads@frs.sourceforge.net:/home/frs/project/pixys-os/ten/"${DEVICE}"/
+   scp -P 5615 -o StrictHostKeyChecking=no "${FILENAME}" root@uploads.pixysos.com:/home/ftp/uploads/"${DEVICE}"/ten/
+   scp -o StrictHostKeyChecking=no "${FILENAME}" pixysuploads@frs.sourceforge.net:/home/frs/project/pixys-os/ten/"${DEVICE}"/
    TG "${FILENAME} has been uploaded to <a href=\"https://downloads.sourceforge.net/project/pixys-os/ten/${DEVICE}/${FILENAME}\">Sourceforge</a> and <a href=\"https://downloads.pixysos.com/${DEVICE}/ten/${FILENAME}\">FTP</a>"
    rm -rf *.zip
 }
