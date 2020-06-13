72090bb665b970207176a963c5ce89cc8d0ecd17
Shreejoy Dash <shreejoy@pixelexperience.org>
2020-06-14 00:06:33 +0530

    More changes
    * changed targets URL
    * now show success and failed count
    * improvise strings

--- scripts/executor.py
+++ scripts/executor.py
@@ -21,9 +21,11 @@ TG_ADMIN = "-1001180192731"
 TG_MAINTAINER = "-1001144148166"
 
 def count_builds(today, device):
-  JENKINS_BUILDS_API = 'https://jenkins.pixysos.com/job/PixysOS-Ten/api/json?tree=builds[timestamp,actions[parameters[name,value]]]&pretty=true'
+  JENKINS_BUILDS_API = 'https://jenkins.pixysos.com/job/PixysOS-Ten/api/json?tree=builds[timestamp,result,actions[parameters[name,value]]]&pretty=true'
   api = requests.get(JENKINS_BUILDS_API)
   count = 0
+  failed = 0
+  success = 0
   if api.status_code == 200:
     api = json.loads(api.text)
     for build in api['builds']:
@@ -36,8 +38,12 @@ def count_builds(today, device):
               if parameter['name'] == 'DEVICE':
                 if device == parameter['value']:
                   count = count + 1
+                  if build["result"] == 'FAILED':
+                    failed = failed + 1
+                  elif build["result"] == 'SUCCESS':
+                    success = success + 1
                   break
-  return count
+  return count, failed, success
 
 device = os.environ.get('device')
 version = os.environ.get('version')
@@ -48,7 +54,7 @@ password = os.environ.get('password')
 jenkins_url = 'https://' + username + ':' + password + '@jenkins.pixysos.com'
 server = jenkins.Jenkins(jenkins_url)
 
-targets_url = 'https://raw.githubusercontent.com/PixysOS/PixysOS_jenkins/ten/pixysos-build-targets'
+targets_url = 'https://raw.githubusercontent.com/PixysOS/official_devices/ten/pixys-build-target'
 targets = requests.get(targets_url).text
 for target in targets.splitlines():
   if device in target:
@@ -60,13 +66,13 @@ for target in targets.splitlines():
 current = datetime.datetime.now().strftime("%s")
 today = datetime.date.fromtimestamp(int(current)).strftime('%Y-%m-%d')
 day = datetime.date.fromtimestamp(int(current)).strftime('%A')[0:2]
-count = count_builds(today, device)
+count, failed, success = count_builds(today, device)
 
 if day == allowed_day and count <= int(allowed_count):
   server.build_job('PixysOS-Ten',{'DEVICE':device, 'pixys_edition':version, 'clean_device':clean_device, 'make_clean':make_clean})
-  message = 'Build triggered for ' + device + ' (' + version + ')' + '.\n\n' + str(int(allowed_count) - int(count) - 1) + ' builds left out of ' + str(allowed_count)
+  message = 'Build triggered for ' + device + ' (' + version + ')' + '.\n\n' + str(int(allowed_count) - int(count) - 1) + ' builds left out of ' + str(allowed_count) + '\n\nFailed :- ' + str(failed) + '\nSuccess' + str(success)
 else:
-  message = 'Build trigger failed for ' + device + ' (' + version + ')' + '. The requested device has either exceeded its quota or its not allowed to be made today. Please refer to build targets https://github.com/PixysOS/PixysOS_jenkins/blob/ten/pixysos-build-targets.'
+  message = 'Build trigger failed for ' + device + ' (' + version + ')' + '. The requested device has either exceeded its quota or its not allowed to be made today. Please refer to build targets https://github.com/PixysOS/official_devices/blob/ten/pixys-build-target.'
 
 print(message)
 UPDATER.bot.send_message(chat_id=TG_ADMIN, text=message, parse_mode='HTML', disable_web_page_preview='yes')
