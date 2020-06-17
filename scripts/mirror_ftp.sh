9a43124e5b0643efc8109507aa4364754f8e7598
Shreejoy Dash <shreejoy@pixelexperience.org>
2020-06-17 13:45:33 +0530

    Update mirror_ftp.sh
    * Now auto push json to github
    * post releases to channel automatically

--- scripts/mirror_ftp.sh
+++ scripts/mirror_ftp.sh
@@ -20,13 +20,32 @@ function TG() {
 
 function mirror() {
    echo "$FILENAME" | grep -q "GAPPS" && FOLDER="ten_gapps" || FOLDER="ten"
+   TG "Initializing the release of <code>${FILENAME}</code> for device. View progress <a href=\"${BUILD_URL}\">here</a>"
    cp /home/ftp/uploads/.test/"${DEVICE}"/${FOLDER}/"${FILENAME}" /home/ftp/ft-uploads/
    CHECK=$(ls PixysOS*.zip)
    [ "${CHECK}" == "${FILENAME}" ] && echo "${FILENAME} found, Starting upload process" || TG "$FILENAME cannot be downloaded correctly"
    mv /home/ftp/uploads/.test/"${DEVICE}"/${FOLDER}/"${FILENAME}" /home/ftp/uploads/"${DEVICE}"/ten/
    sshpass -p "${spass}" scp -o StrictHostKeyChecking=no "${FILENAME}" pixysuploads@frs.sourceforge.net:/home/frs/project/pixys-os/ten/"${DEVICE}"/
-   TG "${FILENAME} has been uploaded to <a href=\"https://downloads.sourceforge.net/project/pixys-os/ten/${DEVICE}/${FILENAME}\">Sourceforge</a> and <a href=\"https://downloads.pixysos.com/${DEVICE}/ten/${FILENAME}\">FTP</a>"
-   rm -rf *.zip
+   git clone git@github.com:PixysOS-Devices/official_devices -b ten
+   cd official_devices
+      mkdir -p ${DEVICE}
+      cp /home/ftp/uploads/.test/"${DEVICE}"/${FOLDER}/"${DEVICE}.json" "${DEVICE}"
+      if [ $FOLDER = "ten"];
+      then
+         rm -rf "${DEVICE}/build.json"
+         mv "${DEVICE}/${DEVICE}.json" "${DEVICE}/build.json"
+      elif [ $FOLDER = "ten_gapps"];
+      then
+         rm -rf "${DEVICE}/gapps_build.json"
+         mv "${DEVICE}/${DEVICE}.json" "${DEVICE}/gapps_build.json"
+      fi
+      git add .
+      git commit -m "official_devices: ${DEVICE} (${FOLDER}) update"
+      git push
+   cd ..
+   wget https://raw.githubusercontent.com/PixysOS/PixysOS_jenkins/ten/scripts/post_message.py
+   python3 post_message.py
+   rm -rf *.zip official_devices post_message.py
 }
 
 mirror
